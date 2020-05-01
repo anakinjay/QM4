e93e177c153d6ff61cf5b4d56d6dc75999a930e8
Slay Jay <anakinjay@gmail.com>
2020-05-01 13:37:39 -0400

    Updates

---
+++ precache-manifest.dc0f1dc0c9bd5a15e1ac5ff29a7b4632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b53c73bd1c22886abe7c2bcd8583ce3",
+    "url": "/QM4/index.html"
+  },
+  {
+    "revision": "a5d44219fba38131c1ea",
+    "url": "/QM4/static/css/2.286c9144.chunk.css"
+  },
+  {
+    "revision": "96889be2f6d2ff6ff7ae",
+    "url": "/QM4/static/css/main.80f7173a.chunk.css"
+  },
+  {
+    "revision": "a5d44219fba38131c1ea",
+    "url": "/QM4/static/js/2.837bdcb0.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/QM4/static/js/2.837bdcb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96889be2f6d2ff6ff7ae",
+    "url": "/QM4/static/js/main.4ba72136.chunk.js"
+  },
+  {
+    "revision": "8e86730f546b339d6868",
+    "url": "/QM4/static/js/runtime-main.c9eac7b5.js"
+  }
+]);
